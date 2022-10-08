29493a9c39a044cfbafc82a8d526c9b35e1ab154
Andrew <andrey777-novikov777@yandex.ru>
2022-10-08 21:16:16 +0300

    changed to be PEP-8 approved.

--- Antiplagiat/main.py
+++ Antiplagiat/main.py
@@ -4,21 +4,26 @@ from itertools import zip_longest
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 
+
 class Window(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         uic.loadUi('main.ui', self)
-        
+
         self.submitButton.clicked.connect(self.onClick)
-    
+
     def onClick(self):
-        leftWords = [elem.strip() for elem in self.leftTextField.toPlainText().split('\n') if elem]
-        rightWords = [elem.strip() for elem in self.rightTextField.toPlainText().split('\n') if elem]
+        leftWords = [elem.strip()
+                     for elem in self.leftTextField.toPlainText().split('\n') if elem]
+        rightWords = [
+            elem.strip() for elem in self.rightTextField.toPlainText().split('\n') if elem]
 
         possibilities = list()
         for line1, line2 in zip(leftWords, rightWords):
-            forwardScan = list(map(lambda ltrs: ltrs[0] == ltrs[1], zip_longest(line1, line2)))
-            backwardScan = list(map(lambda ltrs: ltrs[0] == ltrs[1], zip_longest(line1[::-1], line2[::-1])))
+            forwardScan = list(
+                map(lambda ltrs: ltrs[0] == ltrs[1], zip_longest(line1, line2)))
+            backwardScan = list(
+                map(lambda ltrs: ltrs[0] == ltrs[1], zip_longest(line1[::-1], line2[::-1])))
 
             poss1 = forwardScan.count(True) / len(forwardScan)
             poss2 = backwardScan.count(True) / len(backwardScan)
@@ -29,27 +34,22 @@ class Window(QMainWindow):
             totalPlagiatpossibility = sum(possibilities) / len(possibilities)
 
             message = f'Код похож на {round(totalPlagiatpossibility * 100, 2)}%'
-            if totalPlagiatpossibility  >= self.percentageInput.value() / 100:
+            if totalPlagiatpossibility >= self.percentageInput.value() / 100:
                 message += ' - Это плагиат.'
             self.statusbar.showMessage(message)
 
             redComponent = round(255 * totalPlagiatpossibility)
             greenComponent = round(255 * (1 - totalPlagiatpossibility))
-            
+
             self.statusbar.setStyleSheet(f"""
                 background-color: rgb({redComponent}, {greenComponent}, 0);
             """)
-
         else:
             self.statusbar.showMessage('Нам нечего сверять =)')
             self.statusbar.setStyleSheet("""
                 background-color: rgb(101, 255, 5);
             """)
-        
-                
 
-
-        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
